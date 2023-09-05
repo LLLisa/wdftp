4d0b961fd43602c1986e9f41268605bf54ee6e8b
Lisa Knox <lisaknox83@gmail.com>
2023-09-05 09:08:00 -0400

    separated markdown component

--- src/Root.js
+++ src/Root.js
@@ -1,17 +1,10 @@
 import React from "react";
-import { ReactMarkdown as Markdown } from "react-markdown/lib/react-markdown";
-
-import test from "./lessons/testlesson/test.md";
+import Test from "./views/test";
 
 export default () => {
     return (
         <>
-            <h1>please work</h1>
-            <ul>
-                <li>
-                    <Markdown>{test}</Markdown>
-                </li>
-            </ul>
+            <Test />
         </>
     );
 };
